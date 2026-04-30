ca365775b2226558f2366b53556a322f9ce79531
远程打卡APP <mengshiming2110@163.com>
2026-05-01 04:07:07 +0800

    Release V1.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ defaultConfig {
         applicationId = "com.pengxh.smscheckin"
         minSdk = 26
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.3.1"
+        versionCode = 5
+        versionName = "1.3.2"
     }
 
     signingConfigs {
